1585853fc3915bca9c3a652dbc42d3662686cafe
Kris Allen <i@atdawn.cc>
2023-05-14 10:33:36 +0800

    实现顺序码生成器

--- src/main/java/org/ooohmygosh/utils/SequenceCodeGenerator.java
+++ src/main/java/org/ooohmygosh/utils/SequenceCodeGenerator.java
@@ -1,5 +1,10 @@
 package org.ooohmygosh.utils;
 
+import org.ooohmygosh.model.SequenceCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SequenceCodeGenerator {
     public static enum GENDER {
         ALL,
@@ -7,12 +12,99 @@ public class SequenceCodeGenerator {
         FEMALE
     }
 
-    private int[] maleCodes = new int[]{1,3,5,7,9};
-    private int[] femaleCodes = new int[]{2,4,6,8};
+    private static final int[] maleCodes = new int[]{1,3,5,7,9};
+    private static final int[] femaleCodes = new int[]{2,4,6,8};
+    private static final int[] genderCodes = new int[]{1,2,3,4,5,6,7,8,9};
 
     private GENDER gender = GENDER.ALL;
 
-    public static SequenceCodeGenerator of(){
-        return null;
+    List<SequenceCode> sequenceCodes;
+
+    public static SequenceCodeGenerator builder(){
+        SequenceCodeGenerator sequenceCodeGenerator = new SequenceCodeGenerator();
+        sequenceCodeGenerator.sequenceCodes = new ArrayList<>();
+        return sequenceCodeGenerator;
     }
+
+    public static SequenceCodeGenerator ofRange(int min,int max,GENDER gender){
+        int[] genderCodes = null;
+        switch (gender){
+            case ALL:
+                genderCodes = SequenceCodeGenerator.genderCodes;
+                break;
+
+            case MALE:
+                genderCodes = SequenceCodeGenerator.maleCodes;
+                break;
+
+            case FEMALE:
+                genderCodes = SequenceCodeGenerator.femaleCodes;
+        }
+
+        SequenceCodeGenerator sequenceCodeGenerator = SequenceCodeGenerator.builder();
+
+        for(int i=min;i<max;i++){
+            for (int genderCode : genderCodes) {
+                sequenceCodeGenerator.sequenceCodes.add(
+                        SequenceCode.builder()
+                                .setSequenceCode(i)
+                                .setGenderCode(genderCode)
+                );
+            }
+        }
+
+        return sequenceCodeGenerator;
+    }
+
+    public static SequenceCodeGenerator ofCodes(int[] codes,GENDER gender){
+        int[] genderCodes = null;
+        switch (gender){
+            case ALL:
+                genderCodes = SequenceCodeGenerator.genderCodes;
+                break;
+
+            case MALE:
+                genderCodes = SequenceCodeGenerator.maleCodes;
+                break;
+
+            case FEMALE:
+                genderCodes = SequenceCodeGenerator.femaleCodes;
+        }
+
+        SequenceCodeGenerator sequenceCodeGenerator = SequenceCodeGenerator.builder();
+
+        for (int code : codes) {
+            for (int genderCode : genderCodes) {
+                sequenceCodeGenerator.sequenceCodes.add(
+                        SequenceCode.builder()
+                                .setSequenceCode(code)
+                                .setGenderCode(genderCode)
+                );
+            }
+        }
+
+        return sequenceCodeGenerator;
+    }
+
+    public SequenceCodeIterator iterator(){
+        return new SequenceCodeIterator(this.getSequenceCodes());
+    }
+
+    public int[] getMaleCodes() {
+        return maleCodes;
+    }
+
+    public int[] getFemaleCodes() {
+        return femaleCodes;
+    }
+
+    public GENDER getGender() {
+        return gender;
+    }
+
+    public List<SequenceCode> getSequenceCodes() {
+        return sequenceCodes;
+    }
+
+
 }
